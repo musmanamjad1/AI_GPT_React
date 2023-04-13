691a10bc5a173f191f2daf4362b0cae2eb0bc019
Muhammad Usman Amjad <muhammadusmanamjad1@gmail.com>
2023-04-13 23:07:02 +0500

    Assets Uploading and web structuring

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+//import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+//import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./Navbar.css";
+import logo from "../../assets/logo.svg";
 //BEM - Block Element  Modifiers
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links"></div>
+      <div className="gpt3__navbar-links-logo">
+        <img src={logo} alt="Logo" />
+      </div>
     </div>
   );
 };
