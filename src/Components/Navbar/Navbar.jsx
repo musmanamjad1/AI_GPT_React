c48bc6eca78980256072c37883352d6d0b274f32
Muhammad Usman Amjad <muhammadusmanamjad1@gmail.com>
2023-04-14 00:03:34 +0500

    nav bar Styling and setting

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,8 +8,29 @@ const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links"></div>
-      <div className="gpt3__navbar-links-logo">
+      <div className="gpt3__navbar-links_logo">
         <img src={logo} alt="Logo" />
+      </div>
+      <div className="gpt3__navbar-links_container">
+        <p>
+          <a href="#home">Home</a>
+        </p>
+        <p>
+          <a href="#wgpt3">What is GPT3?</a>
+        </p>
+        <p>
+          <a href="#possibility">Open AI</a>
+        </p>
+        <p>
+          <a href="#features">Case Studies</a>
+        </p>
+        <p>
+          <a href="#blog">Library</a>
+        </p>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type="button"> Sign Up</button>
       </div>
     </div>
   );
