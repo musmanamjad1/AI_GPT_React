60bb2b34dd38355fb714fb7d7a230c190f023224
Muhammad Usman Amjad <muhammadusmanamjad1@gmail.com>
2023-04-15 23:01:58 +0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-//import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
-//import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./Navbar.css";
+
 import logo from "../../assets/logo.svg";
+
 //BEM - Block Element  Modifiers
+
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
